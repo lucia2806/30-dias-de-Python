5f46c959ebde6bf4cf9614dd8139a2d9b14e6572
usuario <luciarojasote@gmail.com>
2023-01-13 10:52:29 +0100

    Added day one and two

---
+++ dia-2/variables.py
@@ -0,0 +1,46 @@
+# Day 2: 30 Days of python programming
+first_name = 'Lucia'
+last_name = 'Rojas'
+full_name = 'Lucia Rojas'
+country_name = 'España'
+city_variable = 'Jerez'
+age_variable = '16'
+year_variable = '2022'
+is_married = False
+is_true = True
+is_light_on = True
+'Lucía Rojas Lucía Rojas Esapaña Jerez 16 2022'
+
+type('Lucía')
+type('Rojas')
+type('Lucia Rojas')
+type('España')
+type('Jerez')
+type('16')
+type('2022')
+len(first_name)
+print(len(full_name))
+num_one=5
+num_two=4
+total = num_one + num_two
+diff = num_one - num_two
+product = nun_one * num_two
+division = num_one / num_two
+floor_division = num_one * mum_two
+exp = num_one , num_two
+remainder = num_two % num_one
+r = 30
+pi = 3,14
+area_of_circle = pi * r ** 2
+circum_of_circle = 2 * pi * r
+r = input("cual es el radio:")
+print(f"el radio es "30"")
+frist_name = input ("cual es el frist name")
+print("the frist neame is Lucia")
+last_name = input ("what is the last name")
+print(f"the last name is Rojas Otero")
+country = input ("what is the contry")
+print(f"the country is España")
+age = input("what is the age")
+print(f"the age is 16")
+help("keywords")
