1d680876e7bb196648abb3976dcaae15d12bb777
usuario <luciarojasote@gmail.com>
2023-02-10 11:09:46 +0100

    dia-4 no terminado

---
+++ dia-4/string.py
@@ -0,0 +1,21 @@
+space = " "
+word1 = "thirty"
+word2 = "days"
+word3 = "of"
+word4 = "python"
+string1 = word1 + space + word2 + space + word3 + space + word4
+word5 = "coding"
+word6 = "for"
+word7 = "all"
+string2 = word5 + space + word6 + space + word7
+company = string2
+print(company)
+print(len(company))
+company.uper()
+company.lower()
+company.capitalize()
+company.title()
+company.swapcase()
+string3 = string2 - word5
+company.index(word5)
+company.replace("coding","python")
