279d6a5dc1191fdc0a02d1f35d44b3d03b5c4f69
usuario <luciarojasote@gmail.com>
2023-02-16 09:09:58 +0100

    dia-4 incompleto

--- dia-4/string.py
+++ dia-4/string.py
@@ -19,3 +19,31 @@ company.swapcase()
 string3 = string2 - word5
 company.index(word5)
 company.replace("coding","python")
+company.split()
+word9 = "to"
+string3 = word8 + space + word9 +space + word4
+word10 = "all"
+string.replace("python","all")
+company.split()
+word11 = "Facebook"
+word12 = "Google"
+word13 = "Microsoft"
+word14 = "Apple"
+word15 = "IBM"
+word16 = "Oracle"
+word17 = "Amazon"
+string4 = word11 + word12 + word13 + word14 + word15 + word16 +word17
+string4.split(",")
+first_letter = string[0]
+last_word = string[14]
+word_10 = string[10]
+PFE = word4 + space + word6 + space + word8
+CFA = word5 + space + word6 + space + word7
+string2.index[1]
+string2.index[2]
+string2.index[3]
+word18 = "people"
+string5 = word4 + space + word6 + space + word8 + space + word18
+string5.rfind()
+string6 =  'You cannot end a sentence with because because because is a conjunction'
+string6.rindex("because")
